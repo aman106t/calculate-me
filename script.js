0393439b0fcb977d3b7ec229d0b6cfcc5a887283
aman106t <142651271+aman106t@users.noreply.github.com>
2023-08-19 11:39:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click',(e)=>{
+    if(e.target.innerHTML =='='){
+      string= eval(string);
+    }
+    else if(e.target.innerHTML =='C'){
+      string= "0";
+    }
+    else if(e.target.innerHTML =='M+'){
+      string= eval(string)+string;
+    }
+    else if(e.target.innerHTML =='M-'){
+      string= eval(string)-(string/2);
+    }
+    else if(e.target.innerHTML =='Del'){
+      string= "";
+    }
+    else{
+    console.log(e.target)
+    string = string+e.target.innerHTML;}
+    document.querySelector('input').value = string;
+  })
+  
+})
